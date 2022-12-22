99d3e0fc8c55e7bce6b7728a098b8ea4272aaae3
samratmiddha <s_middha@me.iitr.ac.in>
2022-12-22 13:21:47 +0530

    material ui and redux setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import themes from "./theme";
+import { createTheme, ThemeProvider } from "@mui/material";
+import Dashboard from "./pages/Dashboard";
 function App() {
+  const theme = () =>
+    createTheme({
+      palette: themes["Light"],
+    });
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
