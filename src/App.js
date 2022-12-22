c9999e2d971bf3cef0fd596334b4f8d1b877899f
samratmiddha <s_middha@me.iitr.ac.in>
2022-12-22 15:24:06 +0530

    home page title and buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import themes from "./theme";
-import { createTheme, ThemeProvider } from "@mui/material";
 import Dashboard from "./pages/Dashboard";
 function App() {
-  const theme = () =>
-    createTheme({
-      palette: themes["Light"],
-    });
-
   return (
-    <div className="App">
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,129 @@
+import {
+  Typography,
+  Button,
+  CardActionArea,
+  CardContent,
+  Card,
+} from "@mui/material";
+import { Box } from "@mui/system";
+
 export default function Home() {
-  return <h1>hii</h1>;
+  let logo = require("../assets/red_cross_logo.jpg");
+  return (
+    <Box>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ paddingLeft: "2rem", paddingTop: "2rem", display: "flex" }}>
+          <img src={logo} alt="Red Cross Logo" style={{ width: "8rem" }} />
+          <Typography
+            variant="h3"
+            sx={{ alignSelf: "center", marginLeft: "1rem" }}
+          >
+            Red Cross Computer Center,Mansa
+          </Typography>
+        </Box>
+        <Box sx={{ alignSelf: "center", paddingRight: "3rem" }}>
+          <Button variant="contained" size="large">
+            Log In
+          </Button>
+        </Box>
+      </Box>
+      <hr></hr>
+      <Box></Box>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box sx={{ display: "flex", width: "60%", flexWrap: "wrap" }}>
+          <Card
+            sx={{
+              width: "15rem",
+              height: "10rem",
+              backgroundColor: "#98D7C2",
+              margin: "4rem",
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            <CardActionArea>
+              <Box
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  alignContent: "center",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignSelf: "center",
+                }}
+              >
+                <Typography variant="h4" sx={{ alignSelf: "center" }}>
+                  Courses
+                </Typography>
+              </Box>
+            </CardActionArea>
+          </Card>
+          <Card
+            sx={{
+              width: "15rem",
+              height: "10rem",
+              backgroundColor: "#98D7C2",
+              margin: "4rem",
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            <CardActionArea>
+              <Box
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  alignContent: "center",
+                  display: "flex",
+                  justifyContent: "center",
+                  flexDirection: "column",
+                  alignSelf: "center",
+                }}
+              >
+                <Typography variant="h4" sx={{ alignSelf: "center" }}>
+                  Cerificate{" "}
+                </Typography>
+                <Typography variant="h4" sx={{ alignSelf: "center" }}>
+                  Verification{" "}
+                </Typography>
+              </Box>
+            </CardActionArea>
+          </Card>
+          <Card
+            sx={{
+              width: "15rem",
+              height: "10rem",
+              backgroundColor: "#98D7C2",
+              margin: "4rem",
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            <CardActionArea>
+              <Box
+                sx={{
+                  height: "100%",
+                  width: "100%",
+                  alignContent: "center",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignSelf: "center",
+                }}
+              >
+                <Typography variant="h4" sx={{ alignSelf: "center" }}>
+                  Apply Online
+                </Typography>
+              </Box>
+            </CardActionArea>
+          </Card>
+        </Box>
+      </Box>
+      <Box>
+        <Typography variant="h5" align="center">
+          Our Meritorius Students
+        </Typography>
+        <Box></Box>
+      </Box>
+    </Box>
+  );
 }
